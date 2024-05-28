cb008a36e0b3d015f97fefcfa2645c4d66a44922
t0nylombardi <alombardi.331@gmail.com>
2024-05-28 12:05:04 -0400

    solution

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -1,12 +1,15 @@
-import { Link } from "react-router-dom";
+import { Link, useOutletContext } from "react-router-dom";
 
-function UserCard({user}) {
-    return (
-      <article>
-          <h2>{user.name}</h2>
-          <Link to={`/profile/${user.id}`}>View profile</Link>
-      </article>
-    );
-  };
-  
-  export default UserCard;
+function UserCard({ user }) {
+  const users = useOutletContext();
+  console.log(users);
+
+  return (
+    <article>
+      <h2>{user.name}</h2>
+      <Link to={`/profile/${user.id}`}>View profile</Link>
+    </article>
+  );
+}
+
+export default UserCard;
